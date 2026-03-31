1e60512a605522adac927c494234efa276311f6d
highlrang <59989932+highlrang@users.noreply.github.com>
2026-03-31 16:50:45 +0900

    init.

---
+++ src/main/java/com/otaservice/otaservice/OtaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.otaservice.otaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otaservice/otaservice/OtaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otaservice.otaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
